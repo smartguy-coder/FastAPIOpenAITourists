107a6ede4acb14d95dc049425ea917516bde7018
Vasyl <vasyl@example.com>
2025-07-05 01:16:26 +0300

    add nginx

--- backend/app/app_factory.py
+++ backend/app/app_factory.py
@@ -7,7 +7,8 @@ from settings import settings
 def get_application() -> FastAPI:
     app = FastAPI(
         title=settings.APP_NAME,
-        debug=settings.DEBUG
+        debug=settings.DEBUG,
+        root_path="/api",
     )
 
     return app
